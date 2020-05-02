2a9bbff0cf49197e0b9fe1f5a579de3bb50c21d0
Vaibhav Chaudhari <vaibhavchaudhari8625@gmail.com>
2020-05-02 16:32:09 +0530

    Updates

---
+++ precache-manifest.b14d7639f236ce5e47ae42c788ece8ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f6d63141b57ad2f6f81a89c82cf2639",
+    "url": "/react-todo-app/index.html"
+  },
+  {
+    "revision": "cbd9dbc70fbc9a643d07",
+    "url": "/react-todo-app/static/css/main.8449a4de.chunk.css"
+  },
+  {
+    "revision": "9c6263cba8e076dafaa2",
+    "url": "/react-todo-app/static/js/2.1dbabc98.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo-app/static/js/2.1dbabc98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbd9dbc70fbc9a643d07",
+    "url": "/react-todo-app/static/js/main.f6a585b5.chunk.js"
+  },
+  {
+    "revision": "6c176b2d402ac355e4e4",
+    "url": "/react-todo-app/static/js/runtime-main.9fb7c203.js"
+  }
+]);
